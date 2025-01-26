d51b472983acd13e08ad4721ed3a876d47ae27d6
elr4dp7 <alexander.Chumaslov@mckesson.com>
2025-01-26 20:10:31 +0300

    Add back button to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,19 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBackHome = () => {
+  router.push({ name: 'home' })
+}
+</script>
+
 <template>
   <div class="about">
     <h2>Пока вы можете только отправить мне USDT(TRC 20) на этот адрес:</h2>
     <img src="../assets/usdt.png" alt="QRcode" class="qr" />
     <span class="address">TUa3GHF7V1TEAX2RfW8poLjdB4CyxuTfRn</span>
+    <button @click="goBackHome" class="button">Вернуться назад</button>
   </div>
 </template>
 
@@ -14,8 +25,6 @@
   text-align: center;
   min-height: 100vh;
   display: flex;
-  /* justify-content: center; */
-  /* align-items: center; */
   flex-direction: column;
 }
 .qr {
@@ -24,5 +33,37 @@
 
 .address {
   font-size: 12px;
+}
+
+.button {
+  background: linear-gradient(
+    145deg,
+    #ffcc00,
+    #b8860b
+  ); /* Градиент от светло золотого к темно золотому */
+  color: #000;
+  font-size: 1rem;
+  font-weight: bold; /* Сделано более жирным */
+  padding: 15px 30px;
+  width: 200px;
+  margin: 20px auto;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  box-shadow:
+    3px 3px 6px #d4af37,
+    -3px -3px 6px #d4af37; /* Желтоватое свечение */
+  transition: all 0.3s;
+}
+
+.button:hover {
+  background: linear-gradient(
+    145deg,
+    #b8860b,
+    #ffcc00
+  ); /* Инвертированный градиент при наведении */
+  box-shadow:
+    5px 5px 10px #d4af37,
+    -5px -5px 10px #d4af37; /* Более интенсивное свечение при наведении */
 }
 </style>
