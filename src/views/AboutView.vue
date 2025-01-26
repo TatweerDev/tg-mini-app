e079b17ed1f608c87720171a6efe3a4de08d6178
elr4dp7 <alexander.Chumaslov@mckesson.com>
2025-01-26 20:03:13 +0300

    Add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Пока вы можете только отправить мне USDT(TRC 20) на этот адрес:</h2>
+    <img src="../assets/usdt.png" alt="QRcode" class="qr" />
+    <span class="address">TUa3GHF7V1TEAX2RfW8poLjdB4CyxuTfRn</span>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  background-color: #000;
+  color: #ffd700; /* Золотой цвет */
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+  min-height: 100vh;
+  display: flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+  flex-direction: column;
+}
+.qr {
+  margin: 30px 0;
+}
+
+.address {
+  font-size: 12px;
 }
 </style>
